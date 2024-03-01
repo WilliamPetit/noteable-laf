6c8c046cdb862818f88cfd5b05191ae962358bcf
Ian Stuart <Ian.Stuart@ed.ac.uk>
2024-03-01 13:45:20 +0000

    Another working version, with a menu

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ import { DOMUtils } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
 
+interface IMyCommand {
+  command: string;
+  label: string;
+  caption: string;
+  icon?: string;
+  action(): void;
+}
+
 const TOP_AREA_CSS_CLASS = 'jp-TopAreaText';
 
 /**
@@ -26,9 +34,28 @@ const plugin: JupyterFrontEndPlugin<void> = {
     menu: IMainMenu
   ) => {
     const command = 'noteable-laf:go2LaunchPage';
+    const myCommands: Array<IMyCommand> = [];
+    myCommands.push({
+      command: 'noteable-laf:go2LaunchPage',
+      label: 'Return to the Launch page',
+      caption: 'Return to the Launch page (to shut your notebook down)',
+      action: () => window.location.assign('/launch')
+    });
+    myCommands.push({
+      command: 'noteable-laf:go2Resources',
+      label: 'View Resources page',
+      caption: 'Open the Noteable Resources page (in a new tab)',
+      action: () => window.open('/resources/')
+    });
+    myCommands.push({
+      command: 'noteable-laf:go2HelpAndGuides',
+      label: 'View Help and Guides page',
+      caption: 'Open the Noteable Help and Guides page (in a new tab)',
+      action: () => window.open('help_guides')
+    });
 
     setupIcons(app);
-    setupCommand(app, palette, command);
+    setupCommands(app, palette, myCommands);
 
     /*
     / Add an item to the File Menu
@@ -79,38 +106,41 @@ function setupIcons(app: JupyterFrontEnd) {
   app.shell.add(widget, 'top', { rank: 1000 });
 }
 
-function setupCommand(
+function setupCommands(
   app: JupyterFrontEnd,
   palette: ICommandPalette,
-  command: string
+  myCommands: Array<IMyCommand>
 ) {
   /*
   / Add a command to the central command-palette
   */
   const { commands } = app;
-
-  // Add a command
-  commands.addCommand(command, {
-    label: 'Return to the Launch page',
-    caption: 'Return to the Launch page (to shut your notebook down)',
-    isEnabled: () => true,
-    isVisible: () => true,
-    execute: (args: any) => {
-      if (args['origin'] !== 'init') {
-        window.location.assign('/launch');
-      }
-    }
-  });
-
   const category = 'Noteable';
-  palette.addItem({ command, category, args: { origin: 'from palette' } });
 
-  // Call the command execution
-  commands.execute(command, { origin: 'init' }).catch((reason) => {
-    console.error(
-      `An error occurred during the execution of ${command}.\n${reason}`
-    );
+  myCommands.forEach(myCommand => {
+    const command = myCommand.command;
+
+    // Add a command
+    commands.addCommand(command, {
+      label: myCommand.label,
+      caption: myCommand.caption,
+      isEnabled: () => true,
+      isVisible: () => true,
+      execute: (args: any) => {
+        if (args['origin'] !== 'init') {
+          myCommand.action();
+        }
+      }
+    });
+
+    palette.addItem({ command, category, args: { origin: 'from palette' } });
+
+    // Call the command execution
+    commands.execute(command, { origin: 'init' }).catch(reason => {
+      console.error(
+        `An error occurred during the execution of ${command}.\n${reason}`
+      );
+    });
   });
 }
-
 export default plugin;
